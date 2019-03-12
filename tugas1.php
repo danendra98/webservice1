2105b47b3ac08f042a325aaaed77ba20215e5dfa
Davis Danendra <48470926+danendra98@users.noreply.github.com>
2019-03-12 15:43:31 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> TUGAS 1</title>
+</head>
+<body>
+<?php 
+   echo "Hello World <br>";
+   $nama="Davis Danendra S. <br>";
+   $nim="16.01.53.0119";
+   echo "Nama 	:".$nama;
+   echo "NIM 	:".$nim;
+?>
+</body>
+</html>
+
